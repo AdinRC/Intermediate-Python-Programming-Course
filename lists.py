724f4f1413aee2f979a7726850c97c1b90fbe89b
AdinRC <93084433+AdinRC@users.noreply.github.com>
2023-01-20 12:41:36 +0800

    first commit

---
+++ lists.py
@@ -0,0 +1,87 @@
+#  Lists : ordered, mutable, allows duplicate elements
+mylist = ["banana", "cherry", "apple"]
+# print(mylist)
+
+# mylist2 = list()
+# print(mylist2)
+
+# mylist2 = [5, True, "apple"]
+# print(mylist2)
+
+# item = mylist[0]
+# print(item)
+
+# for i in mylist:
+#     print(i)
+    
+# if "banana" in mylist:
+#     print("yes")
+# else:
+#     print("no")
+
+# print(len(mylist))
+
+# mylist.append("lemon")
+# print(mylist)
+# mylist.insert(1, "blueberry")
+# print(mylist)
+
+# item = mylist.pop()
+# print(item)
+# print(mylist)
+
+# item = mylist.remove("cherry")
+# print(mylist)
+
+# item = mylist.clear()
+# print(mylist)
+
+# item = mylist.reverse()
+# print(mylist)
+
+# mylist = [4,3,1,-1,-5,10]
+# item = mylist.sort()
+# print(mylist)
+
+# new_list = sorted(mylist)
+# print(mylist)
+# print(new_list)
+
+# mylist = [0] * 5
+# print(mylist)
+# mylist2 = [1,2,3,4,5]
+# new_list = mylist + mylist2
+# print(new_list)
+
+# mylist = [1,2,3,4,5,6,7,8,9]
+## [start dari index:berhenti sebelum di index]
+# a = mylist [:5]
+# print(a)
+# a = mylist [1:5]
+# print(a)
+# a = mylist [::1]
+# print(a)
+# a = mylist [::2]
+# print(a)
+# a = mylist [::3]
+# print(a)
+# a = mylist [::-1]
+# print(a)
+
+# list_org = ["banana", "cherry" , "apple"]
+
+# list_copy = list_org.copy()
+# list_copy.append("lemon")
+# print(list_copy)
+# print(list_org)
+
+# list_copy = list(list_org)
+# list_copy.append("lemon")
+# print(list_copy)
+# print(list_org)
+
+# mylist = [1,2,3,4,5,6]
+# b = [i*i for i in mylist]
+
+# print(mylist)
+# print(b)
